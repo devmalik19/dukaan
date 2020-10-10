09a0347bc70e3c56f79f272b40667a9cce9347bc
deepak <6713928+devmalik19@users.noreply.github.com>
2020-10-10 18:03:07 +0530

    First Commit

---
+++ src/main/java/devmalik90/dukaan/controllers/Products.java
@@ -0,0 +1,34 @@
+package devmalik90.dukaan.controllers;
+
+public class Products 
+{
+    public void getAll()
+    {
+
+    }
+
+    public void get()
+    {
+
+    }
+
+    public void create()
+    {
+
+    }
+
+    public void update()
+    {
+
+    }
+
+    public void delete()
+    {
+
+    }
+
+    public void search()
+    {
+        
+    }
+}
